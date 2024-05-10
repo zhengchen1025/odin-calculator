8a56ad7065299165c91380ef98296680300978f4
ZhengChen <zhengchen1025@gmail.com>
2024-05-10 13:36:01 +0800

    basically works

--- scripts.js
+++ scripts.js
@@ -14,111 +14,109 @@ function divide(a, b) {
   result = a / b;
 }
 
-const num1 = document.getElementById("1");
-const num2 = document.getElementById("2");
-const num3 = document.getElementById("3");
-const num4 = document.getElementById("4");
-const num5 = document.getElementById("5");
-const num6 = document.getElementById("6");
-const num7 = document.getElementById("7");
-const num8 = document.getElementById("8");
-const num9 = document.getElementById("9");
-const num0 = document.getElementById("0");
+const nums = document.querySelectorAll(".num");
 
-const operAdd = document.getElementById("add");
-const operSubtract = document.getElementById("subtract");
-const operMultiply = document.getElementById("multiply");
-const operDivide = document.getElementById("divide");
-const operEquals = document.getElementById("equals");
-const operClear = document.getElementById("clear");
+nums.forEach((num) => {
+  num.addEventListener("click", inputNum);
+});
+
+const opers = document.querySelectorAll(".operator");
+
+opers.forEach((oper) => {
+  oper.addEventListener("click", inputOper);
+});
 
 const inputText = document.getElementById("inputText");
 
-num1.addEventListener("click", inputNum);
-num2.addEventListener("click", inputNum);
-num3.addEventListener("click", inputNum);
-num4.addEventListener("click", inputNum);
-num5.addEventListener("click", inputNum);
-num6.addEventListener("click", inputNum);
-num7.addEventListener("click", inputNum);
-num8.addEventListener("click", inputNum);
-num9.addEventListener("click", inputNum);
-num0.addEventListener("click", inputNum);
+const operEquals = document.getElementById("equals");
+const operClear = document.getElementById("clear");
+const operNegation = document.getElementById("negation");
 
-operAdd.addEventListener("click", inputOper);
-operSubtract.addEventListener("click", inputOper);
-operMultiply.addEventListener("click", inputOper);
-operDivide.addEventListener("click", inputOper);
-operEquals.addEventListener("click", getResult);
 operClear.addEventListener("click", allClear);
+operNegation.addEventListener("click", negate);
 
-let firstNum = null;
-let secondNum = null;
-let result = null;
+function allClear() {
+  inputText.textContent = "";
+  firstNum = "";
+  secondNum = "";
+  calSymbol = "";
+}
+
+function negate() {
+  inputText.textContent *= -1;
+}
+
+// main content
+
+let firstNum = undefined;
+let secondNum = undefined;
+let result;
 let calSymbol = "";
 
 function inputNum(e) {
-  console.log(e);
-  inputText.textContent += e.target.id;
+  if (e.target.textContent === "." && inputText.textContent.includes(".")) {
+    return;
+  }
+  inputText.textContent += e.target.textContent;
 }
 
 function saveFirstNum() {
-  firstNum = parseInt(inputText.textContent);
+  firstNum = parseFloat(inputText.textContent);
   console.log(`firstNum: ${firstNum}`);
 }
 
 function saveSecondNum() {
-  secondNum = parseInt(inputText.textContent);
+  secondNum = parseFloat(inputText.textContent);
   console.log(`secondNum: ${secondNum}`);
-}
-
-function saveCalSymbol() {
-  calSymbol = inputText.textContent;
-  console.log(calSymbol);
-}
-
-function allClear() {
-  inputText.textContent = "";
 }
 
 function inputOper(e) {
   switch (e.target.id) {
     case "add":
       saveFirstNum();
-      console.log(inputText.textContent);
-      inputText.textContent = "+";
-      saveCalSymbol();
-
+      calSymbol = "+";
+      inputText.textContent = "";
+      console.log(
+        `first: ${firstNum}, sec: ${secondNum}, sybl: ${calSymbol} rel: ${result}`
+      );
       break;
     case "subtract":
       saveFirstNum();
-      console.log(inputText.textContent);
-      inputText.textContent = "-";
-      saveCalSymbol();
-
+      calSymbol = "-";
+      inputText.textContent = "";
+      console.log(
+        `first: ${firstNum}, sec: ${secondNum}, sybl: ${calSymbol} rel: ${result}`
+      );
       break;
     case "multiply":
       saveFirstNum();
-      console.log(inputText.textContent);
-      inputText.textContent = "x";
-      saveCalSymbol();
-
+      calSymbol = "x";
+      inputText.textContent = "";
+      console.log(
+        `first: ${firstNum}, sec: ${secondNum}, sybl: ${calSymbol} rel: ${result}`
+      );
       break;
     case "divide":
       saveFirstNum();
-      console.log(inputText.textContent);
-      inputText.textContent = "%";
-      saveCalSymbol();
-
+      calSymbol = "%";
+      inputText.textContent = "";
+      console.log(
+        `first: ${firstNum}, sec: ${secondNum}, sybl: ${calSymbol} rel: ${result}`
+      );
+      break;
+    case "equals":
+      saveSecondNum();
+      compute();
+      calSymbol = "";
+      console.log(
+        `first: ${firstNum}, sec: ${secondNum}, sybl: ${calSymbol} rel: ${result}`
+      );
       break;
   }
 }
 
-function getResult() {
-  saveSecondNum();
-  console.log(`${firstNum} ${calSymbol} ${secondNum}`);
-  console.log(typeof firstNum);
-
+function compute() {
+  if (firstNum === "" && secondNum === "") return;
   switch (calSymbol) {
     case "+":
       add(firstNum, secondNum);
@@ -133,7 +131,8 @@ function getResult() {
       divide(firstNum, secondNum);
       break;
   }
-
-  console.log(result);
   inputText.textContent = result;
+  firstNum = result;
+  secondNum = "";
+  calSymbol = "";
 }
